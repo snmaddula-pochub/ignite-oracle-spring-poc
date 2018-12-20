98bb8d21a9b241f761164cab5ead4a1f37c35f66
snmaddula <snmaddula@outlook.com>
2018-12-20 15:17:10 +0530

    Initial Commit

---
+++ src/main/java/snmaddula/pochub/ios/IgniteOracleSpringApp.java
@@ -0,0 +1,13 @@
+package snmaddula.pochub.ios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteOracleSpringApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteOracleSpringApp.class, args);
+	}
+
+}
